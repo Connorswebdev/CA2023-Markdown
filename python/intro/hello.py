fea605b0d81692b706c205cb5f9eaf6fac1115cb
Connor <connor.ross132@gmail.com>
2023-10-05 12:05:35 +1000

    bash changes

--- python/intro/hello.py
+++ python/intro/hello.py
@@ -1,3 +1,7 @@
-print ("What is your name?")
-my_name = input()
-print ("Hello, " + my_name)
+# print ("What is your name?")
+# my_name = input()
+# print ("Hello, " + my_name)
+
+my_name = input('What is your name?')
+my_age = input('What is your age?')
+print(f'Hello, {my_name} you are {my_age + 10} years old in 10 years')
